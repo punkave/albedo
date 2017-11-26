8989244b4592e35026cfac78279ae8089162b373
Michael Kopinsky <Michael.Kopinsky@uphs.upenn.edu>
2017-11-25 21:57:57 -0500

    Whitespace

--- test/albedo.js
+++ test/albedo.js
@@ -1,12 +1,12 @@
 var should = require('chai').should(),
-    albedo = require('../albedo'),
-    processReport = albedo.processReport;
+albedo = require('../albedo'),
+processReport = albedo.processReport;
 
 var options = {
-    location: "reports",
-    name: "report",
-    query: "SELECT * FROM albedo",
-    connection: {
+  location: "reports",
+  name: "report",
+  query: "SELECT * FROM albedo",
+  connection: {
     type: "db",
     host: "localhost",
     user: "user",
@@ -17,23 +17,23 @@ var options = {
 };
 
 describe('processReport', function() {
-    it('call process report without options', function() {
-        processReport(null, function (err, doc) {
-            should.exist(err);
-            err.should.equal('processReport requires options, see documentation');
-        });
+  it('call process report without options', function() {
+    processReport(null, function (err, doc) {
+      should.exist(err);
+      err.should.equal('processReport requires options, see documentation');
     });
-    it('call process report unsupported DB', function() {
-        processReport(options, function (err, doc) {
-            should.exist(err);
-            err.should.equal('The selected database type is not yet supported');
-        });
+  });
+  it('call process report unsupported DB', function() {
+    processReport(options, function (err, doc) {
+      should.exist(err);
+      err.should.equal('The selected database type is not yet supported');
     });
-    it('call process report', function() {
-        options.connection.type = 'mysql';
-        processReport(options, function (err, doc) {
-            should.not.exist(err);
-        });
+  });
+  it('call process report', function() {
+    options.connection.type = 'mysql';
+    processReport(options, function (err, doc) {
+      should.not.exist(err);
     });
+  });
 });
 
